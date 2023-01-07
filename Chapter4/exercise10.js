71257563887c814134601610cafa0440907fee16
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-01-07 17:25:32 -0500

    Chapter4 Update

---
+++ Chapter4/exercise10.js
@@ -0,0 +1,10 @@
+/*
+multiply(g), left(l), right(l)
+product(l), left(l), right(l)
+product(l)
+getNumber(g), prompt(l)
+parseFloat(g), question(g), prompt(l)
+left(g), getNumber(g)
+right(g), getNumber(g)
+console(g), left(g), right(g), multiply(g)
+*/
