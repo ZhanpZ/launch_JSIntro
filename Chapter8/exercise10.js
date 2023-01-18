0ebbf83ba7721265cd792f4d8fd7465fc8d2d91b
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-01-18 16:26:48 -0500

    Update Chapter8

---
+++ Chapter8/exercise10.js
@@ -0,0 +1,4 @@
+/*
+primitive values: 1, 2, "a", "b", false, null
+objects: [1, 2, ["a", ["b", false]], ["a", ["b", false]], ["b", false], {}
+*/
---
+++ Chapter8/exercise9.js
@@ -0,0 +1,22 @@
+let foo = {
+  a: 'hello',
+  b: 'world',
+};
+
+let qux = 'hello';
+
+function bar(argument1, argument2) {
+  argument1.a = 'hi';
+  argument2 = 'hi';
+}
+
+bar(foo, qux);
+
+console.log(foo.a);
+console.log(qux);
+
+/*
+hi
+hello
+the foo would be mutable because it is an object but you can not mutate qux which is a string
+*/
