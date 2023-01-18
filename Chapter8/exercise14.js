113dc5d286db03152bb719743302809b9653f1cd
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-01-18 16:40:35 -0500

    Update Chapter8

---
+++ Chapter8/exercise14.js
@@ -0,0 +1,6 @@
+/*
+variables: hello, greeting, name, xyzzy, howdy, foo
+object property names: a, b, c, d, also 0, 1, 2 for the array [3,4,5]
+primitive values: ' ', 1, 2, 3, 4, 5, 'Hi', 'Grace', 'a', 'b', 'c', 'd'
+objects: hello, xyzzy
+*/
---
+++ Chapter8/exercise15.js
@@ -0,0 +1,6 @@
+/*
+variables: bar, arg1, arg2, foo, qux, result
+object property names: abc, def, ghi, jkl, mno, pqr, 0,1,2,3
+primitive values: 'Hello', 1,2,3,4,5,6,null,NaN,'Victor','Antonina', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqr', '0', '1', '2', '3'
+objects: bar, qux
+*/
